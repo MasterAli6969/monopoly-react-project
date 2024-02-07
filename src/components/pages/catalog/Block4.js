d359d78c8f30a8eb2de08ef9596fc1e0b52c1217
Али Хамидов <mars.mir6969@gmail.com>
2024-02-07 11:16:43 +0300

    Страница каталога готова, html зачищен, рендер на тестовых данных

--- src/components/pages/catalog/Block4.js
+++ src/components/pages/catalog/Block4.js
@@ -1,36 +1,23 @@
 //<!--------------------- Пагинация ------------------->
 import React from "react";
+import { paginateData } from "./data";
 function Block4() {
   return (
     <>
       <div class="col-12 mb-5 d-flex justify-content-center">
         <nav aria-label="Page navigation example">
           <ul class="pagination">
-            <li class="page-item">
-              <a class="page-link" href="catalog">
-                Previous
-              </a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="catalog">
-                1
-              </a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="catalog">
-                2
-              </a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="catalog">
-                3
-              </a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="catalog">
-                Next
-              </a>
-            </li>
+            {paginateData.map((item) => {
+              return (
+                <>
+                  <li key={item.id} class="page-item">
+                    <a class="page-link" href={item.href}>
+                      {item.pagin}
+                    </a>
+                  </li>
+                </>
+              );
+            })}
           </ul>
         </nav>
       </div>
