a0909e53eefe55de661dd78adab7e05fbb6951ac
Али Хамидов <mars.mir6969@gmail.com>
2024-02-12 15:27:34 +0300

    ДЕПЛОЙ ПРОЕКТА НА GiTUB PAGES

--- src/utils/api/config.js
+++ src/utils/api/config.js
@@ -1,6 +1,4 @@
-import axios from "axios";
-
 // export const BASEURL = "http://162.0.234.100:5095";
-console.log("kncdmmkldsc sd", BASEURL);
 
 export const BASEURL = "https://api.monopoly-card.online";
+console.log("kncdmmkldsc sd", BASEURL);
