bca6a4ec923fdcc5649f83a99ae7240654e389a5
MasterAli6969 <mars.mir6969@gmail.com>
2024-02-28 18:00:26 +0300

    исправление ошибок и установка условий

--- src/components/pages/card-page/CardPage.jsx
+++ src/components/pages/card-page/CardPage.jsx
@@ -8,30 +8,38 @@ import RepliesAccordion from "./RepliesAccordion";
 import RecomendCard from "./RecomendCard";
 import OtherDescriptions from "./OtherDescriptions";
 import { renderCardPageData } from "../../../services/api/apiCatalog";
-//import { dataCard } from "./data";
+
 function CardPage() {
   const [card, setCard] = useState({});
+
   useEffect(() => {
     const getData = async () => {
       const cardData = await renderCardPageData();
       setCard(cardData);
-      console.log("ТУТ ДОЛЖНЫ БЫТЬ ДАННЫЕ О КАРТЕ", card);
+      console.log("ТУТ ДОЛЖНЫ БЫТЬ ДАННЫЕ О КАРТЕ", cardData);
     };
     getData();
   }, []);
+
   return (
     <>
       <div className="container-fluid p-0">
         <div className="container _cardPage">
           <div className="row justify-content-between">
-            <Breadcrumbs />
-            <CardSlider dataImg={card.data && card.data.image_url} />
-            <CardDescriptions dataCard={card.data && card.data} />
-            <WhoForm />
-            <WhoDataForm />
-            <RepliesAccordion />
-            <RecomendCard />
-            <OtherDescriptions />
+            {Object.keys(card).length !== 0 ? (
+              <>
+                <Breadcrumbs />
+                <CardSlider dataImg={card.data && card.data.image_url} />
+                <CardDescriptions dataCard={card.data && card.data} />
+                <WhoForm />
+                <WhoDataForm />
+                <RepliesAccordion />
+                <RecomendCard />
+                <OtherDescriptions />
+              </>
+            ) : (
+              <h1>402</h1>
+            )}
           </div>
         </div>
       </div>
