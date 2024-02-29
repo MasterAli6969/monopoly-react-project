993efe5e52473e9863814c0bc3f624b940644161
Sult23 <sultan.nurmatov@gmail.com>
2024-02-29 17:52:59 +0300

    new

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -2,13 +2,13 @@ import Head from './Head'
 import Slider from './Slider'
 import ModalTabs from './ModalTabs'
 import RecomendCard from './RecomendCard'
-import IconLincsList from './IconLincsList'
 import InstructionCardUse from './InstructionCardUse'
 import Banner from './Banner'
 import CardFeatures from './CardFeatures'
 import OurClients from './OurClients'
 import FeedbackSlider from './FeedbackSlider'
 import '../../../assets/css/index.css'
+import IconLinksList from './IconLinksList'
 
 function Home() {
 	return (
