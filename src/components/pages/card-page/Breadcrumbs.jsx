10835b8ca6102d2af8b8bb67871b782b04524ad3
MasterAli6969 <mars.mir6969@gmail.com>
2024-03-01 18:08:05 +0300

    Routing Link fix

--- src/components/pages/card-page/Breadcrumbs.jsx
+++ src/components/pages/card-page/Breadcrumbs.jsx
@@ -1,26 +1,24 @@
 //<!--------------------- Хлебные крошки и загаловок ------------------->
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Breadcrumbs() {
-	return (
-		<>
-			<div className='col-12 mb-3'>
-				<nav aria-label='breadcrumb'>
-					<ol className='breadcrumb'>
-						<li className='breadcrumb-item'>
-							<Link to='file:///home/ubuntu/monopoly-front-project/index.html'>
-								Главная
-							</Link>
-						</li>
-						<li className='breadcrumb-item active' aria-current='page'>
-							Подарочные сертификаты и карты
-						</li>
-					</ol>
-				</nav>
-				<h3>Подарочные сертификаты и карты</h3>
-			</div>
-		</>
-	)
+  return (
+    <>
+      <div className="col-12 mb-3">
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <Link to="/catalog/card-page">Главная</Link>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Подарочные сертификаты и карты
+            </li>
+          </ol>
+        </nav>
+        <h3>Подарочные сертификаты и карты</h3>
+      </div>
+    </>
+  );
 }
-export default Breadcrumbs
+export default Breadcrumbs;
--- src/components/pages/card-page/RepliesAccordion.jsx
+++ src/components/pages/card-page/RepliesAccordion.jsx
@@ -1,221 +1,221 @@
 //<!--------------------- АККОРДИОНЫ ------------------->
-import { Link } from 'react-router-dom'
-import { accordionData } from './data'
+import { Link } from "react-router-dom";
+import { accordionData } from "./data";
 function RepliesAccordion() {
-	return (
-		<>
-			<div className='col-12 mb-5'>
-				<div className='d-flex flex-wrap justify-content-between align-items-center mb-5'>
-					<div>
-						<h3 className='mb-3 fw-bolder'>
-							Хотите сделать корпоративный заказ?
-						</h3>
-						<p>
-							Оставьте заявку на сайте и мы свяжемся с вами в ближайшее время
-						</p>
-					</div>
-					<a
-						href='/card-page'
-						className='btn btn-light btn-lg rounded-5 over-accent-color text-accent-colors'
-					>
-						Оставить заявку
-					</a>
-				</div>
-				<div className='accordion' id='accordionExample'>
-					{accordionData.map(item => {
-						return (
-							<>
-								{item.id === 1 && (
-									<div key={item.id} id={item.idTeg} className='accordion-item'>
-										<h2 className='accordion-header'>
-											<button
-												className='accordion-button collapsed bg-accent-colors'
-												type='button'
-												data-bs-toggle='collapse'
-												data-bs-target={`#${item.dataBsTarget}`}
-												aria-expanded='true'
-												aria-controls={item.dataBsTarget}
-											>
-												{item.accordName}
-											</button>
-										</h2>
-										<div
-											id={item.dataBsTarget}
-											className='accordion-collapse collapse p-3'
-											data-bs-parent='#accordionExample'
-										>
-											<div className='accordion-body d-flex justify-content-center flex-wrap p-0 mb-5'>
-												{item.accordItem.map(item => {
-													return (
-														<>
-															<div
-																key={item.id}
-																className='card'
-																style={{ width: '25rem' }}
-															>
-																<div className='card-body d-flex flex-column align-items-center justify-content-center text-center'>
-																	<img
-																		alt=''
-																		src={item.img}
-																		className='img-thumbnail mb-4'
-																	/>
-																	<h5 className='card-title mb-3'>
-																		{item.title}
-																	</h5>
-																	<p className='card-text'>
-																		{item.description}
-																	</p>
-																</div>
-															</div>
-														</>
-													)
-												})}
-											</div>
-											<div>
-												<p>
-													<strong>Протестируйте:</strong>
-													отправьте себе письмо с примером
-												</p>
-											</div>
-											<form className='d-flex justify-content-between align-items-center flex-wrap'>
-												<div className='mb-3'>
-													<label
-														htmlFor='exampleInputPassword1'
-														className='form-label'
-													>
-														Имя
-													</label>
-													<input
-														type='text'
-														className='form-control'
-														id='exampleInputPassword1'
-													/>
-												</div>
-												<div className='mb-3'>
-													<label
-														htmlFor='exampleInputEmail1'
-														className='form-label'
-													>
-														Email*
-													</label>
-													<input
-														type='email'
-														className='form-control'
-														id='exampleInputEmail1'
-														aria-describedby='emailHelp'
-													/>
-												</div>
-												<button
-													type='button'
-													className='btn btn-light btn-lg rounded-5 over-accent-color text-accent-colors'
-												>
-													Далее
-													<i className='bi bi-arrow-right-circle-fill text-accent-colors'></i>
-												</button>
-											</form>
-										</div>
-									</div>
-								)}
+  return (
+    <>
+      <div className="col-12 mb-5">
+        <div className="d-flex flex-wrap justify-content-between align-items-center mb-5">
+          <div>
+            <h3 className="mb-3 fw-bolder">
+              Хотите сделать корпоративный заказ?
+            </h3>
+            <p>
+              Оставьте заявку на сайте и мы свяжемся с вами в ближайшее время
+            </p>
+          </div>
+          <Link
+            href="/card-page"
+            className="btn btn-light btn-lg rounded-5 over-accent-color text-accent-colors"
+          >
+            Оставить заявку
+          </Link>
+        </div>
+        <div className="accordion" id="accordionExample">
+          {accordionData.map((item) => {
+            return (
+              <>
+                {item.id === 1 && (
+                  <div key={item.id} id={item.idTeg} className="accordion-item">
+                    <h2 className="accordion-header">
+                      <button
+                        className="accordion-button collapsed bg-accent-colors"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#${item.dataBsTarget}`}
+                        aria-expanded="true"
+                        aria-controls={item.dataBsTarget}
+                      >
+                        {item.accordName}
+                      </button>
+                    </h2>
+                    <div
+                      id={item.dataBsTarget}
+                      className="accordion-collapse collapse p-3"
+                      data-bs-parent="#accordionExample"
+                    >
+                      <div className="accordion-body d-flex justify-content-center flex-wrap p-0 mb-5">
+                        {item.accordItem.map((item) => {
+                          return (
+                            <>
+                              <div
+                                key={item.id}
+                                className="card"
+                                style={{ width: "25rem" }}
+                              >
+                                <div className="card-body d-flex flex-column align-items-center justify-content-center text-center">
+                                  <img
+                                    alt=""
+                                    src={item.img}
+                                    className="img-thumbnail mb-4"
+                                  />
+                                  <h5 className="card-title mb-3">
+                                    {item.title}
+                                  </h5>
+                                  <p className="card-text">
+                                    {item.description}
+                                  </p>
+                                </div>
+                              </div>
+                            </>
+                          );
+                        })}
+                      </div>
+                      <div>
+                        <p>
+                          <strong>Протестируйте:</strong>
+                          отправьте себе письмо с примером
+                        </p>
+                      </div>
+                      <form className="d-flex justify-content-between align-items-center flex-wrap">
+                        <div className="mb-3">
+                          <label
+                            htmlFor="exampleInputPassword1"
+                            className="form-label"
+                          >
+                            Имя
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <label
+                            htmlFor="exampleInputEmail1"
+                            className="form-label"
+                          >
+                            Email*
+                          </label>
+                          <input
+                            type="email"
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                          />
+                        </div>
+                        <button
+                          type="button"
+                          className="btn btn-light btn-lg rounded-5 over-accent-color text-accent-colors"
+                        >
+                          Далее
+                          <i className="bi bi-arrow-right-circle-fill text-accent-colors"></i>
+                        </button>
+                      </form>
+                    </div>
+                  </div>
+                )}
 
-								{item.id === 2 && (
-									<div key={item.id} id={item.idTeg} className='accordion-item'>
-										<h2 className='accordion-header'>
-											<button
-												className='accordion-button collapsed bg-accent-colors'
-												type='button'
-												data-bs-toggle='collapse'
-												data-bs-target={`#${item.dataBsTarget}`}
-												aria-expanded='false'
-												aria-controls={item.dataBsTarget}
-											>
-												{item.accordName}
-											</button>
-										</h2>
-										<div
-											id={item.dataBsTarget}
-											className='accordion-collapse collapse p-3'
-											data-bs-parent='#accordionExample'
-										>
-											<div className='accordion-body'>
-												<div className='mb-5'>
-													<h3 className='fw-bolder mb-3'>
-														{item.accordItem.title}
-													</h3>
-													<p className='mb-3'>{item.accordItem.description1}</p>
-													<p className='p-0 m-0'>
-														{item.accordItem.description2}
-													</p>
-													<p className='p-0 m-0'>
-														{item.accordItem.description3}
-														<Link to='/card-page'>здесь</Link>.
-													</p>
-												</div>
-												<div className='d-flex justify-content-center flex-wrap p-0 mb-5'>
-													{item.accordItem.tablets.map(item => {
-														return (
-															<>
-																<div
-																	key={item.id}
-																	className='card p-5 mx-2'
-																	style={{ width: '20rem' }}
-																>
-																	<div className='card-body d-flex flex-column align-items-center justify-content-center text-center'>
-																		<i className={`${item.icons} fs-1`}></i>
-																		<p className='card-text'>
-																			{item.description}
-																		</p>
-																	</div>
-																</div>
-															</>
-														)
-													})}
-												</div>
-											</div>
-										</div>
-									</div>
-								)}
+                {item.id === 2 && (
+                  <div key={item.id} id={item.idTeg} className="accordion-item">
+                    <h2 className="accordion-header">
+                      <button
+                        className="accordion-button collapsed bg-accent-colors"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#${item.dataBsTarget}`}
+                        aria-expanded="false"
+                        aria-controls={item.dataBsTarget}
+                      >
+                        {item.accordName}
+                      </button>
+                    </h2>
+                    <div
+                      id={item.dataBsTarget}
+                      className="accordion-collapse collapse p-3"
+                      data-bs-parent="#accordionExample"
+                    >
+                      <div className="accordion-body">
+                        <div className="mb-5">
+                          <h3 className="fw-bolder mb-3">
+                            {item.accordItem.title}
+                          </h3>
+                          <p className="mb-3">{item.accordItem.description1}</p>
+                          <p className="p-0 m-0">
+                            {item.accordItem.description2}
+                          </p>
+                          <p className="p-0 m-0">
+                            {item.accordItem.description3}
+                            <Link to="/card-page">здесь</Link>.
+                          </p>
+                        </div>
+                        <div className="d-flex justify-content-center flex-wrap p-0 mb-5">
+                          {item.accordItem.tablets.map((item) => {
+                            return (
+                              <>
+                                <div
+                                  key={item.id}
+                                  className="card p-5 mx-2"
+                                  style={{ width: "20rem" }}
+                                >
+                                  <div className="card-body d-flex flex-column align-items-center justify-content-center text-center">
+                                    <i className={`${item.icons} fs-1`}></i>
+                                    <p className="card-text">
+                                      {item.description}
+                                    </p>
+                                  </div>
+                                </div>
+                              </>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
 
-								{item.id === 3 && (
-									<div key={item.id} id={item.idTeg} className='accordion-item'>
-										<h2 className='accordion-header'>
-											<button
-												className='accordion-button collapsed bg-accent-colors'
-												type='button'
-												data-bs-toggle='collapse'
-												data-bs-target={`#${item.dataBsTarget}`}
-												aria-expanded='false'
-												aria-controls={item.dataBsTarget}
-											>
-												{item.accordName}
-											</button>
-										</h2>
-										<div
-											id={item.dataBsTarget}
-											className='accordion-collapse collapse'
-											data-bs-parent='#accordionExample'
-										>
-											<div className='accordion-body p-5'>
-												<ol>
-													{item.accordItem.map(item => {
-														return (
-															<>
-																<li key={item.id} className='mb-3'>
-																	<p>{item.text}</p>
-																</li>
-															</>
-														)
-													})}
-												</ol>
-											</div>
-										</div>
-									</div>
-								)}
-							</>
-						)
-					})}
-				</div>
-			</div>
-		</>
-	)
+                {item.id === 3 && (
+                  <div key={item.id} id={item.idTeg} className="accordion-item">
+                    <h2 className="accordion-header">
+                      <button
+                        className="accordion-button collapsed bg-accent-colors"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#${item.dataBsTarget}`}
+                        aria-expanded="false"
+                        aria-controls={item.dataBsTarget}
+                      >
+                        {item.accordName}
+                      </button>
+                    </h2>
+                    <div
+                      id={item.dataBsTarget}
+                      className="accordion-collapse collapse"
+                      data-bs-parent="#accordionExample"
+                    >
+                      <div className="accordion-body p-5">
+                        <ol>
+                          {item.accordItem.map((item) => {
+                            return (
+                              <>
+                                <li key={item.id} className="mb-3">
+                                  <p>{item.text}</p>
+                                </li>
+                              </>
+                            );
+                          })}
+                        </ol>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
-export default RepliesAccordion
+export default RepliesAccordion;
