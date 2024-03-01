7d71b3fe072f307c41770c91eade3b9e2674c068
Sult23 <sultan.nurmatov@gmail.com>
2024-03-01 15:05:40 +0300

    new

--- src/components/pages/card-page/CardPage.jsx
+++ src/components/pages/card-page/CardPage.jsx
@@ -10,13 +10,11 @@ import RecomendCard from './RecomendCard'
 import OtherDescriptions from './OtherDescriptions'
 
 function CardPage() {
-	useEffect =
-		(() => {
-			const reduxCardData = localStorage.getItem('cardInfo')
-			let parsedData = reduxCardData ? JSON.parse(reduxCardData) : null
-			console.log('ВАМ ТУТ ПРИШЛА КАКАЯ ТО ХacsascЕРНЯ', parsedData)
-		},
-		[])
+	useEffect(() => {
+		const reduxCardData = localStorage.getItem('cardInfo')
+		let parsedData = reduxCardData ? JSON.parse(reduxCardData) : null
+		console.log('ВАМ ТУТ ПРИШЛА КАКАЯ ТО ХacsascЕРНЯ', parsedData)
+	}, [])
 
 	return (
 		<>
