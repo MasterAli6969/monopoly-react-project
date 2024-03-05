dc1ec0dc2f417240717cd06eb7909a2e81c1d737
MasterAli6969 <mars.mir6969@gmail.com>
2024-03-05 21:01:48 +0300

    Bag fix

--- src/components/pages/card-page/CardPage.jsx
+++ src/components/pages/card-page/CardPage.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import Breadcrumbs from "./Breadcrumbs";
 import CardSlider from "./CardSlider";
 import CardDescriptions from "./CardDescriptions";
@@ -9,11 +8,11 @@ import RecomendCard from "./RecomendCard";
 import OtherDescriptions from "./OtherDescriptions";
 
 function CardPage() {
-  useEffect(() => {
-    const reduxCardData = localStorage.getItem("cardData");
-    const parsedData = reduxCardData ? JSON.parse(reduxCardData) : null;
-    console.log("ВАМ ТУТ ПРИШЛА КАКАЯ ТО ХacsascЕРНЯ", parsedData);
-  }, []);
+  // useEffect(() => {
+  //   const reduxCardData = localStorage.getItem("cardData");
+  //   const parsedData = reduxCardData ? JSON.parse(reduxCardData) : null;
+  //   console.log("ВАМ ТУТ ПРИШЛА КАКАЯ ТО ХacsascЕРНЯ", parsedData);
+  // }, []);
 
   return (
     <>
