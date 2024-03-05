c3125443192e1a833f232f9f3e61c20164c070f1
MasterAli6969 <mars.mir6969@gmail.com>
2024-03-05 21:19:25 +0300

    Bag fix

--- src/components/pages/card-page/CardPage.jsx
+++ src/components/pages/card-page/CardPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Breadcrumbs from "./Breadcrumbs";
 import CardSlider from "./CardSlider";
 import CardDescriptions from "./CardDescriptions";
@@ -9,11 +9,15 @@ import RecomendCard from "./RecomendCard";
 import OtherDescriptions from "./OtherDescriptions";
 
 function CardPage() {
+  const [cardData, setCardData] = useState({});
   useEffect(() => {
     const reduxCardData = localStorage.getItem("cardData");
     const parsedData = reduxCardData ? JSON.parse(reduxCardData) : null;
     console.log("ВАМ ТУТ ПРИШЛА КАКАЯ ТО ХacsascЕРНЯ", parsedData.data);
-  }, []);
+    if (parsedData && parsedData.data) {
+      setCardData(parsedData.data);
+    }
+  }, [cardData]);
 
   return (
     <>
