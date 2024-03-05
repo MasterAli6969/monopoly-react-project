3cd848f10517b0ab2ccbb6e6bec67eca2caf3319
MasterAli6969 <mars.mir6969@gmail.com>
2024-03-05 21:06:18 +0300

    Bag fix

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,19 +1,19 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-function TooltipInitializer() {
-  useEffect(() => {
-    const tooltipTriggerList = document.querySelectorAll(
-      '[data-bs-toggle="tooltip"]'
-    );
-    const tooltipList = [...tooltipTriggerList].map(
-      /* global bootstrap */
-      (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
-    );
-    return () => {
-      tooltipList.forEach((tooltip) => tooltip.dispose());
-    };
-  }, []);
-  return null;
-}
+// function TooltipInitializer() {
+//   useEffect(() => {
+//     const tooltipTriggerList = document.querySelectorAll(
+//       '[data-bs-toggle="tooltip"]'
+//     );
+//     const tooltipList = [...tooltipTriggerList].map(
+//       /* global bootstrap */
+//       (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+//     );
+//     return () => {
+//       tooltipList.forEach((tooltip) => tooltip.dispose());
+//     };
+//   }, []);
+//   return null;
+// }
 
-export default TooltipInitializer;
+// export default TooltipInitializer;
