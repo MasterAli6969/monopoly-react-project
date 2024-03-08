ac18c451e27dc72bf4e6d163759bfccc8acf5545
MasterAli6969 <mars.mir6969@gmail.com>
2024-03-08 16:00:33 +0300

    Небольшая логика на Card-pade управления колвом и ценой

--- src/components/pages/card-page/CardPage.jsx
+++ src/components/pages/card-page/CardPage.jsx
@@ -28,7 +28,7 @@ function CardPage() {
               <TooltipInitializer />
               <Breadcrumbs />
               <CardSlider dataImg={cardData.image_url} />
-              <CardDescriptions />
+              <CardDescriptions dataCard={cardData} />
               <WhoForm />
               <WhoDataForm />
               <RepliesAccordion />
