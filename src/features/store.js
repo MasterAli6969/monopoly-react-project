2e424a1a70ab08126b8c0df95cf3821643f200c2
MasterAli6969 <mars.mir6969@gmail.com>
2024-03-26 16:09:42 +0300

    Смена редукторов на отдельные

--- src/features/store.js
+++ src/features/store.js
@@ -1,7 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartValueReducer from "./cartValueReducer";
 import cardRenderDataReduce from "./cardRenderDataReduce";
-import shoppinCartRenderReduser from "./shoppinCartRenderReduser";
+import setCardIdReduser from "./shoppin-cart-render-redusers/setCardIdReduser";
+import setCertificateReduser from "./shoppin-cart-render-redusers/setCertificateReduser";
+import setCountDicremReduser from "./shoppin-cart-render-redusers/setCountDicremReduser";
+import setCountIncremReduser from "./shoppin-cart-render-redusers/setCountIncremReduser";
+import setCountToDefaultReduser from "./shoppin-cart-render-redusers/setCountToDefaultReduser";
+import setDenominationReduser from "./shoppin-cart-render-redusers/setDenominationReduser";
+import setRecipientEmailReduser from "./shoppin-cart-render-redusers/setRecipientEmailReduser";
+import setRecipientReduser from "./shoppin-cart-render-redusers/setRecipientReduser";
+import setTotalDicremReduser from "./shoppin-cart-render-redusers/setTotalDicremReduser";
+import setTotalIncremReduser from "./shoppin-cart-render-redusers/setTotalIncremReduser";
+import setTotalReduser from "./shoppin-cart-render-redusers/setTotalReduser";
+
 import storage from "redux-persist/lib/storage";
 import {
   persistStore,
@@ -23,7 +34,17 @@ const persistConfig = {
 const rootReducer = combineReducers({
   cartValueReducer: cartValueReducer,
   cardRenderDataReduce: cardRenderDataReduce,
-  shoppinCartRenderReduser: shoppinCartRenderReduser,
+  setCardIdReduser: setCardIdReduser,
+  setCertificateReduser: setCertificateReduser,
+  setCountDicremReduser: setCountDicremReduser,
+  setCountIncremReduser: setCountIncremReduser,
+  setCountToDefaultReduser: setCountToDefaultReduser,
+  setDenominationReduser: setDenominationReduser,
+  setRecipientEmailReduser: setRecipientEmailReduser,
+  setRecipientReduser: setRecipientReduser,
+  setTotalDicremReduser: setTotalDicremReduser,
+  setTotalIncremReduser: setTotalIncremReduser,
+  setTotalReduser: setTotalReduser,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
